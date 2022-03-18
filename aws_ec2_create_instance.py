aad0e92fac32e2f878e9a2dbd89840bf704c2df6
mgarvey07 <96322897+mgarvey07@users.noreply.github.com>
2022-03-18 01:38:24 -0400

    Update aws_ec2_create_instance.py

--- aws_ec2_create_instance.py
+++ aws_ec2_create_instance.py
@@ -16,4 +16,11 @@ def create_ec2_instance():
     except Exception as e:
         print(e)
 
-
+def describe_ec2_instance():
+    try:
+        print ("Describing EC2 instance")
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
+       
+    except Exception as e:
+        print(e)
